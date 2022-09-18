d42792ae75b0138dab29d12406ea52f9987bd744
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2022-09-18 03:51:36 +0100

    configured swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Weblog API documentation')
+    .setDescription('This is the API for Weblog backend built with NestJS')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
