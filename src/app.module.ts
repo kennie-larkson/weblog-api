13959dbceee74f1b0a693fb57aa10e23c04b9773
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2022-09-19 09:18:51 +0100

    remove AppController and AppService from Appmodule controllers and providers array

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+//import { AppController } from './app.controller';
+//import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [UserModule, PostModule, CommentModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
